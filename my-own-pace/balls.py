82291e2714c522cb0e0fef705b21de1f8a72850d
BeauConstrictor <scabby-gone-boring@duck.com>
2025-10-03 20:44:08 +0100

    big improvements to the bouncing balls simulation

--- my-own-pace/balls.py
+++ my-own-pace/balls.py
@@ -4,10 +4,17 @@ import turtle
 import math
 import time
 
-SIM_SPEED = 0.0001
-SIM_SIZE = 200
-COLLISION_TOLERANCE = 1
+SIM_SPEED = 0.01
 DRAG = 0.9995
+G = 10
+SPAWN_RATE = 200
+BALL_COUNT = 30
+HOUSEKEEPING_FREQ = 1000
+COLOURS = ["#cc241d", "#98971a", "#d79921", "#458588", "#b16286", "#689d6a"]
+BEZEL = "#1d2021"
+BACKGROUND = "#282828"
+
+sim_size = 200
 
 class PhysicsObject:
     def __init__(self, colliders: list[Self]):
@@ -24,24 +31,36 @@ class PhysicsObject:
         
         self.colliders = colliders
         
-    def step(self, dt):
-        self.vy -= self.mass
-        
-        bound = SIM_SIZE - self.mass / 2
-        
-        if self.x >= bound or self.x <= -bound:
-            self.vx *= -self.bounciness
-        if self.y >= bound or self.y <= -bound:
-            self.vy *= -self.bounciness
+    def apply_gravity(self) -> None:
+        self.vy -= self.mass * G
             
-        self.vx *= self.drag
-        self.vy *= self.drag
+    def move_based_on_velocity(self, dt) -> None:
+        bound = sim_size - self.mass / 2
         
         self.x += self.vx * dt
         self.y += self.vy * dt
         
         self.x = sorted([-bound, self.x, bound])[1]
         self.y = sorted([-bound, self.y, bound])[1]
+        
+    def bounce(self) -> None:
+        bound = sim_size - self.mass / 2
+        
+        if self.x >= bound or self.x <= -bound:
+            self.vx *= -self.bounciness
+        if self.y >= bound or self.y <= -bound:
+            self.vy *= -self.bounciness
+        
+    def apply_drag(self) -> None:
+        self.vx *= self.drag
+        self.vy *= self.drag
+        
+    def step(self, dt):
+        self.apply_gravity()
+        self.bounce()
+        self.apply_drag()
+        
+        self.move_based_on_velocity(dt)
         
 class Ball(PhysicsObject):
     def __init__(self, colliders: list[PhysicsObject]):
@@ -50,43 +69,66 @@ class Ball(PhysicsObject):
         self.vx = random.randint(-10000, 10000)
         self.vy = random.randint(-10000, 10000)
         
-        self.x = random.randint(-100, 100)
-        self.y = random.randint(-100, 100)
-        
         self.mass = random.randint(10, 50)
+        self.bounciness = 10 / self.mass
+        
+        self.x = random.randint(-sim_size + self.mass, sim_size - self.mass)
+        self.y = sim_size - self.mass
         
         self.turtle = turtle.Turtle()
         self.turtle.shape("circle")
         self.turtle.shapesize(self.mass / 20)
         self.turtle.penup()
         
-        self.turtle.color(*[random.randint(0, 200) for i in range(3)])
+        self.turtle.color(random.choice(COLOURS))
         
     def draw(self):
         self.turtle.goto(self.x, self.y)
         
+def add_ball(balls: list[PhysicsObject], x: float|None, y: float|None) -> None:
+    removed = balls[0]
+    removed.turtle.hideturtle()
+    balls.remove(removed)
+    
+    ball = Ball(balls)
+    if x: ball.x = x
+    if y: ball.y = y
+    balls.append(ball)
+        
 def main() -> None:
+    global sim_size
+    
     turtle.tracer(0)
     turtle.colormode(255)
-    turtle.bgcolor("black")
+    turtle.bgcolor(BEZEL)
     
     bg = turtle.Turtle()
     bg.shape("square")
-    bg.shapesize(SIM_SIZE / 20 * 2)
-    bg.color("white")
-    bg.stamp()
-    bg.hideturtle()
+    bg.shapesize(sim_size / 20 * 2)
+    bg.color(BACKGROUND)
     
     balls = []
     
-    for i in range(100):
+    for i in range(BALL_COUNT):
         balls.append(Ball(balls))
         
     previous_time = time.perf_counter()
+    
+    turtle.onscreenclick(lambda x, y: add_ball(balls, x, y))
+    
+    iteration = 0
         
     while True:
+        if iteration % HOUSEKEEPING_FREQ == 0:
+            sim_size = min(turtle.window_width(), turtle.window_height()) // 2 - 10
+            bg.shapesize(sim_size / 20 * 2)
+        
+        if SPAWN_RATE != -1 and random.randint(0, SPAWN_RATE) == 0:
+            add_ball(balls, None, None)
+        
         current_time = time.perf_counter()
         elapsed = current_time - previous_time
+        previous_time = current_time
         
         dt = elapsed * SIM_SPEED
                 
@@ -95,6 +137,7 @@ def main() -> None:
             b.draw()
             
         turtle.update()
+        iteration += 1
         
 if __name__ == "__main__":
     main()
